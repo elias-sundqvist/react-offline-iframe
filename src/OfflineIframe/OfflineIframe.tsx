efce260a76b63f9678a82beadcf45b3989693643
Elias Sundqvist <elias@sundqvist.com>
2022-01-02 21:35:06 +0100

    Release 0.0.2

--- src/OfflineIframe/OfflineIframe.tsx
+++ src/OfflineIframe/OfflineIframe.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import defineLocalIframe from "./defineLocalIframe";
 import { mkUrl } from "./utils";
 
-const OfflineIframe = ({ address, fetchUrlContent, getResourceUrl, proxy }) => {
+const OfflineIframe = ({ address, fetchUrlContent, getResourceUrl, proxy, ...props }) => {
   const LocalIframe = defineLocalIframe({fetchUrlContent, getResourceUrl});
   return <div>
     <LocalIframe onload={async ()=>{}} src={address} proxy={proxy} fetchProxy={async ({href, base, contextUrl})=>
       {
         href = proxy(mkUrl(contextUrl, href)).href;
         return await base(href);
-    }} onIframePatch={async ()=>{}}/>
+    }} onIframePatch={async ()=>{}} {...props} />
     </div>
 };
 
